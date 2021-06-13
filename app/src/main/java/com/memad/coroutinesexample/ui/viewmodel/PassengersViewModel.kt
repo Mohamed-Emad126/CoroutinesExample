330532036e6bae69b6614fd4a878716094e6356e
Mohamed Emad <mohamed.emad222210@gmail.com>
2021-06-13 21:00:04 +0200

    Fix Bug: Handle Coroutines Exceptions in ViewModel

--- app/src/main/java/com/memad/coroutinesexample/ui/viewmodel/PassengersViewModel.kt
+++ app/src/main/java/com/memad/coroutinesexample/ui/viewmodel/PassengersViewModel.kt
@@ -1,17 +1,21 @@
 package com.memad.coroutinesexample.ui.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.memad.coroutinesexample.data.Resource
 import com.memad.coroutinesexample.data.model.DataItem
 import com.memad.coroutinesexample.data.repository.PassengersRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class PassengersViewModel(private val passengersRepository: PassengersRepository) : ViewModel() {
+
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, e ->
+        passengersLiveData.postValue(Resource.error("${e.message}", items.value))
+    }
 
     val passengersLiveData = MutableLiveData<Resource<ArrayList<DataItem>>>()
     private val items = MutableLiveData<ArrayList<DataItem>>()
@@ -25,16 +29,21 @@ class PassengersViewModel(private val passengersRepository: PassengersRepository
     fun getPassengers(page: Int = 0) {
         currentPage.postValue(page)
         passengersLiveData.postValue(Resource.loading(items.value))
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler) {
             val passengersResponse = passengersRepository.getPassengers(page)
             if (passengersResponse.isSuccessful) {
                 items.value?.addAll(passengersResponse.body()!!.data)
-                withContext(Dispatchers.Main){
+                withContext(Dispatchers.Main) {
                     lastPage.value = passengersResponse.body()!!.totalPages
                 }
                 passengersLiveData.postValue(Resource.success(items.value))
             } else {
-                passengersLiveData.postValue(Resource.error("Error: ${passengersResponse.message()}", items.value))
+                passengersLiveData.postValue(
+                    Resource.error(
+                        "Error: ${passengersResponse.message()}",
+                        items.value
+                    )
+                )
             }
         }
     }
